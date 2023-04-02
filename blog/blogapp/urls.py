64841d6d53aad4846b5505b3b479b4bee2d080b9
Dijith damodaran <dijithdamodaran@gmail.com>
2023-04-02 17:24:45 +0530

    url mapping passing parameter to url

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -4,4 +4,7 @@ from blogapp import views
 urlpatterns = [
     path('about/',views.about),
     path('contact/',views.contact),
+    path('edit/<rid>',views.edit),
+    path('delete/<rid>',views.delete),
+    path('add/<a>/<b>',views.addition),
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -3,5 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 def about(request):
     return HttpResponse("hello from function based view")
+
 def contact(request):
     return HttpResponse("hello from function based view contact")
+
+def edit(request,rid):
+    return HttpResponse("hello this is edit page:"+rid)
+
+def delete(request,rid):
+    return HttpResponse("this is the delete page:",rid)
+
+def addition(request,a,b):
+    c=int(a)+int(b)
+    return HttpResponse("this is addition :"+str(c))
